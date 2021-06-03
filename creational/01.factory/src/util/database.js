4a4e64a2e02f2310710780a82a0586653c9c2e43
wolff453 <george8234@hotmail.com>
2021-06-03 19:17:12 -0300

    commit

--- creational/01.factory/src/util/database.js
+++ creational/01.factory/src/util/database.js
@@ -2,21 +2,7 @@
     constructor({connectionString}){
         this.connectionString = connectionString
     }
-    async sleep(ms){
-        return new Promise(resolve => {
-            setTimeout(resolve,ms)
-        })
-    }
-
-    async connect() {
-           await this.sleep(100)
-        return this //Retorna o this para conseguir usar os métodos dessa classe
-    }
- 
-
-    // e(){
-    //     return console.log("teste")
-    // }
+     
  }
 
  
